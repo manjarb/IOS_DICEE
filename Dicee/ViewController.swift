981de5a0c6250eb71f2b7a74a068ccade0907073
Varis Darasirikul <varis.dara@gmail.com>
2018-08-19 01:45:01 +0800

    dndt

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Varis Darasirikul on 19/8/2561 BE.
+//  Copyright © 2561 Amusexd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
